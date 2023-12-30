9ca7c2af5bb48d0040f4cfb6a93b4b58306fe61c
Mallu <mallusreddy@gmail.com>
2023-12-30 17:01:56 -0600

    commit message

---
+++ src/test/java/com/Tests/googleDemo.java
@@ -0,0 +1,24 @@
+package com.Tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class googleDemo 
+{
+	@Test
+	public void testA() throws InterruptedException
+	{
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Thread.sleep(2000);
+		System.out.println(driver.getTitle());
+		Assert.assertTrue(driver.getTitle().contains("Google"),"Title did not match");
+		driver.close();
+	}
+}
